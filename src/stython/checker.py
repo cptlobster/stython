c110aeb64aeeb388048a5ef98f6cfb8cc1bd1bd7
Dustin Thomas <stdio@cptlobster.dev>
2025-04-09 20:29:18 -0500

    type annotate type checker function

--- src/stython/checker.py
+++ src/stython/checker.py
@@ -1,6 +1,6 @@
+from typing import Union, Type
 
-
-def _isinstance(value: any, typeof: type) -> bool:
+def _isinstance(value: any, typeof: Union[type, Type]) -> bool:
     """check if the value matches the type signature given by typeof."""
     # TODO: subscripted generics cannot be used with class and instance checks
     return isinstance(value, typeof)
